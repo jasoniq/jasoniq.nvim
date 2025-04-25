96702d96ff5e998ee6b9256c6a77a7bae39c7e7c
Jason R. Wendlandt <16008513+jasoniq@users.noreply.github.com>
2025-04-25 06:10:34 -0700

    Fix nvim tree options

--- lua/jasoniq/plugins/files.lua
+++ lua/jasoniq/plugins/files.lua
@@ -1,11 +1,42 @@
+local nvimtreeOpts = {
+  filters = { dotfiles = true },
+  disable_netrw = true,
+  hijack_cursor = true,
+  sync_root_with_cwd = true,
+  update_focused_file = {
+    enable = true,
+    update_root = false,
+  },
+  view = {
+    width = 30,
+    preserve_window_proportions = true,
+  },
+  renderer = {
+    root_folder_label = false,
+    highlight_git = true,
+    indent_markers = { enable = true },
+    icons = {
+      glyphs = {
+        default = "󰈚",
+        folder = {
+          default = "",
+          empty = "",
+          empty_open = "",
+          open = "",
+          symlink = "",
+        },
+        git = { unmerged = "" },
+      },
+    },
+  },
+}
+
 return {
   -- and a directory file explorer that is easy to use
   {
     "nvim-tree/nvim-tree.lua",
     cmd = { "NvimTreeToggle", "NvimTreeFocus" },
-    opts = function()
-      return require "jasoniq.configs.nvimtree"
-    end,
+    opts = nvimtreeOpts,
   },
 
   -- update the file explorer with nice icons
